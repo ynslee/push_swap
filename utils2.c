721dfed9c17c80e29c2ee02c3ed26af879434f80
chuchilini <vanesalees@gmail.com>
2023-05-05 14:36:30 +0300

    --all

--- utils2.c
+++ utils2.c
@@ -6,7 +6,7 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 14:05:03 by yoonslee          #+#    #+#             */
-/*   Updated: 2023/05/05 12:54:57 by yoonslee         ###   ########.fr       */
+/*   Updated: 2023/05/05 14:36:20 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,28 +51,51 @@ void	divide_to_b(t_ps *ps, int length)
 	b_before = ps->len_b;
 	if (is_sorted(ps->a, ps->len_a) == 1)
 		return ;
-	if (count <= 3)
-		return (sort_top_a(ps, count));
+	if (length <= 3)
+		return (top_sort_a(ps, length));
 	find_median(ps->a);
-	while (ps->len_b - b_before < count / 2)
-	{
-		if (ps->a[0] < ps->median)
-			pb(ps);
-		else if (ps->a[0] == ps->median && count % 2 == 0)
-			pb(ps);
-		else if (ps->a[0] >= median && ++i)
-			ra(ps);
-	}
-	while (i-- > 0)
-		rra(ps);
-	if (ps->len_b - b_init_size > 0 && ++ps->lvl)
-		add_one_num_front(ps, ps->len_b - b_init_size);
-	divide_a_to_b(ps, ps->len - b_init_size - ps->len_a + count % 2);
+	// while (ps->len_b - b_before < length / 2)
+	// {
+	// 	if (ps->a[0] < ps->median)
+	// 		pb(ps);
+	// 	else if (ps->a[0] == ps->median && length % 2 == 0)
+	// 		pb(ps);
+	// 	else if (ps->a[0] >= ps->median && ++i)
+	// 		ra(ps);
+	// }
+	// while (i-- > 0)
+	// 	rra(ps);
+	// if (ps->len_b - b_before > 0 && ++ps->lvl)
+	// 	add_one_num_front(ps, ps->len_b - b_init_size);
+	// divide_to_b(ps, ps->len - b_before- ps->len_a + count % 2);
 }
 
-void	top_sort_a(t_ps *ps)
+void	top_sort_a(t_ps *ps, int length)
 {
-	
+	if (length == 2)
+		sa(ps);
+	if (length == 3 && ps->a[0] < ps->a[1] && ps->a[0] < ps->a[2])
+	{	
+		pb(ps);
+		sa(ps);
+	}
+	if (ps->count == 3 && ps->b[2] < ps->b[1] && ps->b[2] < ps->b[0])
+	{
+		sa(ps);
+		pb(ps);
+		if (ps->a[0] > ps->a[1])
+			sa(ps);
+	}
+	if (ps->count == 3 && ps->b[1] > ps->b[0] && ps->b[1] > ps->b[2])
+	{
+		if (ps->a[0] > ps->a[1])
+			sa(ps);
+		ra(ps);
+		sa(ps);
+		pb(ps);
+		rra(ps);
+	}
+	pa(ps);
 }
 
 void	find_median_b(t_ps *ps, int len)
